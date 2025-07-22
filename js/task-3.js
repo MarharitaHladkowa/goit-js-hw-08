23bc6c031bd53ba71de4444095a2cbe72c913fea
margarita <margarita198700@gmail.com>
2025-07-22 11:33:40 +0300

    jr

--- js/task-3.js
+++
@@ -1,6 +0,0 @@
-const input = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
-
-input.addEventListener("input", () => {
-  output.textContent = input.value.trim() || "Anonymous";
-});
